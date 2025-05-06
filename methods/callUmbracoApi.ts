e70326a177603a23ce9e44bbd4338cdfbd3cac97
Owain Williams <owain@owain.codes>
2025-05-06 08:11:19 +0100

    Tidy up

--- methods/callUmbracoApi.ts
+++ methods/callUmbracoApi.ts
@@ -3,30 +3,27 @@ import { requestUrl, Notice } from 'obsidian';
 
 export async function CallUmbracoApi(endpoint: string, bearerToken: string,  method = 'GET', body?: any): Promise<any> {
 	
-	const response = await requestUrl({
-		url: `${endpoint}`,
-		method,
-		headers: {
-			'Authorization': `Bearer ${bearerToken}`,
-			'Content-Type': 'application/json',
-		},
-		body: body ? JSON.stringify(body) : undefined,
-	});
+	if (!bearerToken) {
+		new Notice('Bearer token is null. Please check your settings.');
+		return null;
+	}
 
-	if (response.status === 201 || response.status === 200) {
-		try {
-			// Attempt to parse JSON if possible
-			return response.json;
-		} catch (jsonError) {
-			// If JSON parsing fails, log a warning and return the raw response
-			console.warn('Invalid JSON response:', response.text);
-			new Notice('Invalid JSON response from API.');
-			return { ok: true, status: response.status, text: response.text };
-		}
-	} else {
-		// Handle non-OK status codes
-		console.error('API call failed with status:', response.status);
-		new Notice(`API call failed with status: ${response.status}`);
+	try{
+		const response = await requestUrl({
+			url: `${endpoint}`,
+			method,
+			headers: {
+				'Authorization': `Bearer ${bearerToken}`,
+				'Content-Type': 'application/json',
+			},
+			body: body ? JSON.stringify(body) : undefined,
+		});
 
+		return response; // Return the parsed JSON response
+	}
+	catch (error) {
+		console.error('Error in CallUmbracoApi:', error);
+		new Notice('Error in CallUmbracoApi: ' + error.message);
+		return null;
 	}
 }
---
+++ methods/getUmbracoDocType.ts
@@ -0,0 +1,26 @@
+import { Notice } from 'obsidian';
+import { CallUmbracoApi } from './callUmbracoApi';
+
+export async function GetUmbracoDocType(docType: string, websiteUrl: string, token: any): Promise<any> {
+        
+        const endpoint = `${websiteUrl}/umbraco/management/api/v1/item/document-type/search?query=${docType}&skip=0&take=1`;
+        if (token === null) {
+            new Notice('Bearer token is null. Please check your settings.');
+            return null;
+        }
+
+        const obsidianDocTypeRaw = await CallUmbracoApi(endpoint, token, 'GET');
+        if (!obsidianDocTypeRaw) {
+            new Notice('Failed to fetch document type.');
+            return null;
+        }
+
+
+        const jsonDocType = obsidianDocTypeRaw.json;
+        if (jsonDocType && jsonDocType.items && jsonDocType.items.length > 0) {
+            //console.log('Obsidian document type found:', obsidianDocType.items[0]);
+            return jsonDocType.items[0]; // Return the first document type found
+        } else {
+            new Notice('No Obsidian document type found.');
+        }
+    }
